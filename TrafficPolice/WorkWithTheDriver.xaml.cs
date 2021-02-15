059a5ec2a6544e13a108dc0e29b455113fef66b0
ИС-41 <-41@LAB24-01>
2021-02-15 10:46:01 +0300

    SerchDriver

---
+++ TrafficPolice/WorkWithTheDriver.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrafficPolice
+{
+    /// <summary>
+    /// Логика взаимодействия для WorkWithTheDriver.xaml
+    /// </summary>
+    public partial class WorkWithTheDriver : Page
+    {
+        public WorkWithTheDriver()
+        {
+            InitializeComponent();
+            using (MyDBconnection db = new MyDBconnection())
+            {
+                db.Drivers.Load();
+                DriverGrid.ItemsSource = db.Drivers.Local;
+            }
+        }
+    }
+}
