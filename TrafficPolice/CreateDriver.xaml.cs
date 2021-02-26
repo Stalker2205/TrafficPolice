6f4d5b395027f3b2ba7280d28c3dd91d4fe1dcf8
олимп <@LAB17-01>
2021-02-26 16:27:05 +0300

    Add creat driver

--- TrafficPolice/CreateDriver.xaml.cs
+++ TrafficPolice/CreateDriver.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace TrafficPolice
 {
@@ -28,25 +30,88 @@ namespace TrafficPolice
             Photo.Source = bitmapImage;
             PhotoButton.Width = bitmapImage.Width;
         }
-
+        bool key = false;
+        string photoName;
         private void CreateDriverButton_Click(object sender, RoutedEventArgs e)
         {
             #region Check number and Series
             int PasSer, PasNum, LicSer, LicNum;
             if (PassSeries.Text.Length == 0 || PassSeries.Text.Length != 4) { MessageBox.Show("Серия паспорта состоит из 4-х цифр"); return; }
             else { try { PasSer = Convert.ToInt32(PassSeries.Text); } catch { MessageBox.Show("Серия паспорта состоит из 4-х цифр"); return; } }
-            
+
             if (DriverLicenseSeries.Text.Length == 0 || DriverLicenseSeries.Text.Length != 4) { MessageBox.Show("Серия прав состоит из 4-х цифр"); return; }
             else { try { LicSer = Convert.ToInt32(DriverLicenseSeries.Text); } catch { MessageBox.Show("Серия прав состоит из 4-х цифр"); return; } }
-           
-            if(PassNumber.Text.Length == 0 || PassNumber.Text.Length != 6) { MessageBox.Show("Номер паспорта состоит из 6 цифр");return; }
+
+            if (PassNumber.Text.Length == 0 || PassNumber.Text.Length != 6) { MessageBox.Show("Номер паспорта состоит из 6 цифр"); return; }
             else { try { PasNum = Convert.ToInt32(PassNumber.Text); } catch { MessageBox.Show("Номер паспорта состоит из 6 цифр"); return; } }
-           
+
             if (DriverLicenceNumber.Text.Length == 0 || DriverLicenceNumber.Text.Length != 6) { MessageBox.Show("Номер прав состоит из 6 цифр"); return; }
             else { try { PasNum = Convert.ToInt32(DriverLicenceNumber.Text); } catch { MessageBox.Show("Номер прав состоит из 6 цифр"); return; } }
             #endregion
+            #region Check datetime
+            if (StartDate.SelectedDate.Value.Date.Year < 1900) { MessageBox.Show($"Человеку не может быть {DateTime.Now.Date.Year - StartDate.SelectedDate.Value.Date.Year} лет"); return; }
+            if (Vidali.SelectedDate.Value.Date.Year < 1900) { MessageBox.Show($"Человеку не может быть {DateTime.Now.Date.Year - StartDate.SelectedDate.Value.Date.Year} лет"); return; }
+            if ((FinishDate.SelectedDate.Value.Date.Year - StartDate.SelectedDate.Value.Date.Year) > 150) { MessageBox.Show($"На данный момент, человек не может прожить больше 150 лет"); return; }
 
+            #endregion
+            #region Kanegory Mass
+            string[] kategory = new string[16];
+            kategory[0] = "A"; kategory[1] = "A1";
+            kategory[2] = "B"; kategory[3] = "B1";
+            kategory[4] = "C"; kategory[5] = "C1";
+            kategory[6] = "D"; kategory[7] = "D1";
+            kategory[8] = "BE"; kategory[9] = "CE";
+            kategory[10] = "C1E"; kategory[11] = "DE";
+            kategory[12] = "D1E"; kategory[13] = "M";
+            kategory[14] = "Tm"; kategory[15] = "Tb";
+            #endregion
+            #region add Driver,Passport,DriverLicence
+            if (!key) { MessageBox.Show("Необходимо выбрать фото!"); return; }
+            int driverId = 0;
+            using (MyDBconnection db = new MyDBconnection())
+            {
+                Driver driver = new Driver();
+                driver.FirstName = FirstName.Text;
+                driver.LastName = LastName.Text;
+                driver.Patronymic = Patronimic.Text;
+                driver.Photo = photoName;
+                db.Drivers.Add(driver);
+                db.SaveChanges();
+                db.Drivers.Load();
+                var driv = db.Drivers.Local.Where(x => x.Photo == photoName);
+                foreach (Driver dr in driv) { driverId = dr.DriverID; }
+                Passport pass = new Passport();
+                pass.PassportID = driverId;
+                pass.PassportNumber = Convert.ToInt32(PassNumber.Text);
+                pass.PassportSeries = Convert.ToInt32(PassSeries.Text);
+                pass.PassportAdress = Adress.Text;
+                pass.DateOfIssue = Vidali.DisplayDate.Date;
+                db.Passports.Add(pass);
+                DriversLicense driversLicense = new DriversLicense();
+                driversLicense.DriverID = driverId;
+                driversLicense.DriversLicenseNumber = Convert.ToInt32(DriverLicenceNumber.Text);
+                driversLicense.DriversLicenseSeries = Convert.ToInt32(DriverLicenseSeries.Text);
+                driversLicense.Category = Kategory.Text;
+                driversLicense.DateStart = StartDate.DisplayDate.Date;
+                driversLicense.DateEnd = FinishDate.DisplayDate.Date;
+                db.DriversLicenses.Add(driversLicense);
+                db.SaveChanges();
+            }
+            #endregion
+            MessageBox.Show("Добавлено!"); return;
+        }
 
+        private void PhotoButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image (*.png;*.jpeg;*.BMP)|*.png;*.jpeg;*.BMP";
+            if (dialog.ShowDialog() == true)
+            {
+                Photo.Source = new BitmapImage(new Uri(dialog.FileName));
+                File.Copy(dialog.FileName, AppDomain.CurrentDomain.BaseDirectory + $"\\Image\\Photo\\{dialog.SafeFileName}");
+                key = true;
+                photoName = dialog.SafeFileName;
+            }
         }
     }
 }
